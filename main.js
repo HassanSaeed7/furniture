5c43521473a4a199f489db9bcec891c803899414
HassanSaeed7 <Hsaeed1007@gmail.com>
2022-03-21 18:44:12 -0700

    g

--- main.js
+++ main.js
@@ -1,38 +1,38 @@
 $( "#o1" ).on('mouseenter', function() {
-  $('#root').css({"background": 'url(/public/2.jpg', "background-size": "cover", 'background-repeat': 'no-repeat', 'transition': 'all 0.25s ease', });
-  $('#img').attr("src", "/public/Perkins.png");
+  $('#root').css({"background": 'url(/images/2.jpg', "background-size": "cover", 'background-repeat': 'no-repeat', 'transition': 'all 0.25s ease', });
+  $('#img').attr("src", "/images/Perkins.png");
 }).on('mouseleave', function() {
   $('#root').css("background", 'rgb(29, 29, 29)', 'slow').dequeue();
-  $('#img').attr("src", "/public/3.svg");
+  $('#img').attr("src", "/images/3.svg");
 });
 
 
 $( "#o2" ).on('mouseenter', function() {
-  $('#root').css({"background": 'url(/public/3.jpg', "background-size": "cover", 'background-repeat': 'no-repeat', 'transition': 'all 0.25s ease',});
-  $('#img').attr("src", "/public/Shae.png");
+  $('#root').css({"background": 'url(/images/3.jpg', "background-size": "cover", 'background-repeat': 'no-repeat', 'transition': 'all 0.25s ease',});
+  $('#img').attr("src", "/images/Shae.png");
 }).on('mouseleave', function() {
   $('#root').css("background", 'rgb(29, 29, 29)', 'slow').dequeue();
-  $('#img').attr("src", "/public/3.svg");
+  $('#img').attr("src", "/images/3.svg");
 });
 $( "#o3" ).on('mouseenter', function() {
-  $('#root').css({"background": 'url(/public/4.jpg', "background-size": "cover", 'background-repeat': 'no-repeat', 'transition': 'all 0.25s ease',});
-  $('#img').attr("src", "/public/Rohn.png");
+  $('#root').css({"background": 'url(/images/4.jpg', "background-size": "cover", 'background-repeat': 'no-repeat', 'transition': 'all 0.25s ease',});
+  $('#img').attr("src", "/images/Rohn.png");
 }).on('mouseleave', function() {
   $('#root').css("background", 'rgb(29, 29, 29)', 'slow').dequeue();
-  $('#img').attr("src", "/public/3.svg");
+  $('#img').attr("src", "/images/3.svg");
 });
 $( "#o4" ).on('mouseenter', function() {
-  $('#root').css({"background": 'url(/public/5.jpg', "background-size": "cover", 'background-repeat': 'no-repeat', 'transition': 'all 0.25s ease',});
-  $('#img').attr("src", "/public/Vic.png");
+  $('#root').css({"background": 'url(/images/5.jpg', "background-size": "cover", 'background-repeat': 'no-repeat', 'transition': 'all 0.25s ease',});
+  $('#img').attr("src", "/images/Vic.png");
 }).on('mouseleave', function() {
   $('#root').css("background", 'rgb(29, 29, 29)', 'slow').dequeue();
-  $('#img').attr("src", "/public/3.svg");
+  $('#img').attr("src", "/images/3.svg");
 });
 $( "#o5" ).on('mouseenter', function() {
-  $('#root').css({"background": 'url(/public/6.jpg', "background-size": "cover", 'background-repeat': 'no-repeat', 'transition': 'all 0.25s ease',});
-  $('#img').attr("src", "/public/Anvil.png");
+  $('#root').css({"background": 'url(/images/6.jpg', "background-size": "cover", 'background-repeat': 'no-repeat', 'transition': 'all 0.25s ease',});
+  $('#img').attr("src", "/images/Anvil.png");
 }).on('mouseleave', function() {
   $('#root').css("background", 'rgb(29, 29, 29)', 'slow').dequeue();
-  $('#img').attr("src", "/public/3.svg");
+  $('#img').attr("src", "/images/3.svg");
 });
 
